4f18a7086290909a2f22a130965a2ea8981666f0
Rajib Singh <rajibsingh@yahoo.com>
2016-06-15 00:45:18 -0400

    Initial Commit

---
+++ Mandelzoom-MacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mandelzoom-MacOS
+//
+//  Created by Rajib Singh on 6/15/16.
+//  Copyright © 2016 Sepoy Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mandelzoom-MacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mandelzoom-MacOS
+//
+//  Created by Rajib Singh on 6/15/16.
+//  Copyright © 2016 Sepoy Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
